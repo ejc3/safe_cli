e71fbd746b02b4f5c9afd041b4a4e9f1a5550baf
ejc3 <ej.campbell@gmail.com>
2026-09-08 08:46:47 +0000

    transforms_test: gofmt the comment alignment CI rejected

--- cmd/safe_cli/transforms_test.go
+++ cmd/safe_cli/transforms_test.go
@@ -88,8 +88,8 @@ func TestTransformsRejectBadInput(t *testing.T) {
 		{"pause_schedule", "45m", "30m|1h|2h|4h|until-morning"},
 		{"tz_short", "Mars/Olympus", "IANA zone"},
 		{"iso_micro", "yesterday-ish", "not a time"},
-		{"iso_micro", "106752d", "too large"},                      // would overflow time.Duration into the FUTURE
-		{"iso_micro", "99999999999999999999d", "too large"},        // overflows the integer itself
+		{"iso_micro", "106752d", "too large"},               // would overflow time.Duration into the FUTURE
+		{"iso_micro", "99999999999999999999d", "too large"}, // overflows the integer itself
 		{"epoch_ms", "3650000h", "too large"},
 		{"day3_lower", "funday", "not a day name"},
 		{"day3_lower", "mondayx", "not a day name"}, // a prefix match would silently make this mon
